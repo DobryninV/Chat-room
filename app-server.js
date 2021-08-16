a49f0b981a071375d6a4d68f75353049a30078ea
DobryninV <bronzyletkav@yandex.ru>
2021-08-16 14:22:02 +0300

    fix:port

--- app-server.js
+++ app-server.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const port = process.env.PORT || 80;
+
 app.use(express.static(__dirname + "/build"));
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello</h2>');
 });
 
-app.listen(80, () => {
-  console.log("app start on 80 post");
+app.listen(port, () => {
+  console.log(`app start on ${port} port`);
 });
